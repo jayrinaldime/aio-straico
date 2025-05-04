45a34af02ab8933081e98e60d38c8f8836c8998c
JRinaldi <jrinaldi@jayrinaldi.me>
2025-05-04 20:17:39 +0900

    update model list v0

--- example/async_list_models_v0.py
+++ example/async_list_models_v0.py
@@ -9,26 +9,13 @@ from aio_straico.utils import (
 
 async def async_main():
     async with aio_straico_client() as client:
-        models_v1 = await client.models()
-        chat_models, image_models = to_model_enum(models_v1)
+        models_v0 = await client.models(v=0)
+        chat_models = to_model_enum(models_v0)
 
-        pprint(dir(image_models.flux))
-        # image_models.flux.flux_1_1
+        pprint(models_v0)
 
-        pprint(dir(image_models.ideogram))
-        # image_models.ideogram.flux_1_1
-        # image_models.ideogram.ideogram_v_1
-        # image_models.ideogram.ideogram_v_1_turbo
-        # image_models.ideogram.ideogram_v_2
-        # image_models.ideogram.ideogram_v_2_turbo
-        # image_models.ideogram.ideogram_v_2a
-        # image_models.ideogram.ideogram_v_2a_turbo
 
-        pprint(dir(image_models.openai))
-        # image_models.openai.dall_e_3
-
-        image_model_names = to_model_mapping_by_name(models_v1["image"][0])
-        pprint(image_model_names)
+        print(chat_models.openai.gpt_4o_mini)
 
 
 if __name__ == "__main__":
