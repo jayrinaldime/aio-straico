253b7e0544592895548bd83ccf83058916cae448
JRinaldi <jrinaldi@jayrinaldi.me>
2025-05-05 13:35:07 +0900

    add model list filter (#15)

--- example/async_basic_chat_v0.py
+++ example/async_basic_chat_v0.py
@@ -1,5 +1,6 @@
 import asyncio
 from aio_straico import aio_straico_client
+from aio_straico.utils import to_model_enum
 
 
 async def async_main():
@@ -13,5 +14,19 @@ async def async_main():
         print(reply["completion"]["choices"][0]["message"]["content"])
 
 
+async def async_main_enum_model():
+    async with aio_straico_client() as client:
+        models_v1 = await client.models(v=1)
+        chat_models, image_models = to_model_enum(models_v1)
+        reply = await client.prompt_completion(
+            chat_models.amazon.nova_lite_1_0,
+            "Tell me a joke",
+            temperature=2.0,
+            max_tokens=100,
+        )
+        print(reply["completion"]["choices"][0]["message"]["content"])
+
+
 if __name__ == "__main__":
     asyncio.run(async_main())
+    asyncio.run(async_main_enum_model())
